0dd44a1d7921e38a21f085e118a391aa91f68430
Ericsson Almeida <eriblind@gmail.com>
2023-03-15 19:46:30 -0300

    update

--- PessoaFisica/src/PessoaFisica.java
+++ PessoaFisica/src/PessoaFisica.java
@@ -1,107 +1,129 @@
 public class PessoaFisica {
-    
-    private String nome;
-    private String ultimoNome;
-    private char genero;
-    private int idade;
-    private String CPF;
-    private String estadoCivil;
-    private String CEP;
-    
-    public PessoaFisica(String nome, String ultimoNome, char genero, int idade, String CPF, String estadoCivil, String CEP) {
-        this.nome = nome;
-        this.ultimoNome = ultimoNome;
-        this.genero = genero;
-        this.idade = idade;
-        this.CPF = CPF;
-        this.estadoCivil = estadoCivil;
-        setCEP(CEP);
+    public static void main(String[] args) throws Exception {
+        Person pessoa1 = new Person("Asaph", "Souza", "15454545478", "Solteiro", "123456123", 'm', 26);
+        String dadosDaPessoa1 = pessoa1.toString();
+        System.out.println(dadosDaPessoa1);
     }
-    
-    public String getNome() {
-        return nome;
-    }
-    
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    
-    public String getUltimoNome() {
-        return ultimoNome;
-    }
-    
-    public void setUltimoNome(String ultimoNome) {
-        this.ultimoNome = ultimoNome;
-    }
-    
-    public char getGenero() {
-        return genero;
-    }
-    
-    public void setGenero(char genero) {
-        this.genero = genero;
-    }
-    
-    public int getIdade() {
-        return idade;
-    }
-    
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    
-    public String getCPF() {
-        return CPF;
-    }
-    
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
-    
-    public String getEstadoCivil() {
-        return estadoCivil;
-    }
-    
-    public void setEstadoCivil(String estadoCivil) {
-        this.estadoCivil = estadoCivil;
-    }
-    
-    public String getCEP() {
-        return CEP;
-    }
-    
-    public void setCEP(String CEP) {
-        if (CEP.matches("\\d{8}")) {
-            this.CEP = CEP;
-        } else {
-            throw new IllegalArgumentException("CEP deve conter 8 dígitos numéricos.");
-        }
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof PessoaFisica) {
-            PessoaFisica outraPessoa = (PessoaFisica) obj;
-            return this.CPF.equals(outraPessoa.CPF);
-        }
-        return false;
-    }
-    
-    @Override
-    public int hashCode() {
-        return CPF.hashCode();
-    }
+}
+
+class Person{
+    private String name, lastName, cpf, maritalStatus, cep;
+    private char gender;
+    private int age;
     
     @Override
     public String toString() {
-        return "PessoaFisica{" +
-                "nome='" + nome + '\'' +
-                ", ultimoNome='" + ultimoNome + '\'' +
-                ", genero=" + genero +
-                ", idade=" + idade +
-                ", CPF='" + CPF + '\'' +
-                ", estadoCivil='" + estadoCivil + '\'' +
-                ", CEP='" + CEP + '\'' +
-                '}';
+        return "Person [name=" + name + ", lastName=" + lastName + ", cpf=" + cpf + ", maritalStatus=" + maritalStatus
+                + ", cep=" + cep + ", gender=" + gender + ", age=" + age + "]";
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((maritalStatus == null) ? 0 : maritalStatus.hashCode());
+        result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+        result = prime * result + gender;
+        result = prime * result + age;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (maritalStatus == null) {
+            if (other.maritalStatus != null)
+                return false;
+        } else if (!maritalStatus.equals(other.maritalStatus))
+            return false;
+        if (cep == null) {
+            if (other.cep != null)
+                return false;
+        } else if (!cep.equals(other.cep))
+            return false;
+        if (gender != other.gender)
+            return false;
+        if (age != other.age)
+            return false;
+        return true;
+    }
+
+    public Person(String name, String lastName, String cpf, String maritalStatus, String cep, char gender, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.cpf = cpf;
+        this.maritalStatus = maritalStatus;
+        this.cep = cep;
+        this.gender = gender;
+        this.age = age;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
 }
